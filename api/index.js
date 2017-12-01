cf10b0ee1630202f8e7c7b2ea1b7be6e88d8172d
erlaveri <erlaveri@gmail.com>
2017-12-01 07:54:18 +0200

    4

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const notesRouter = require('./notes');
+
+router.all('/notes', notesRouter);
 
 module.exports = router;
